389236896a573dc96d1e050012f11e10e4cebdfd
Huy02082005 <duchuy2341@gmail.com>
2025-12-25 21:24:27 +0700

    Remove comments and add module export

--- customerRoutes.js
+++ customerRoutes.js
@@ -1,12 +1,10 @@
-// customerRoutes.js - ĐẢM BẢO ĐÚNG
 const express = require('express');
 const router = express.Router();
 const customerController = require('../controllers/customerController');
 
-// TẤT CẢ CÁC ROUTES
 router.get('/', customerController.getAllCustomers);
 router.get('/stats', customerController.getCustomerStats);
-router.post('/', customerController.createCustomer);    // QUAN TRỌNG: DÒNG NÀY
+router.post('/', customerController.createCustomer);
 router.put('/:id', customerController.updateCustomer);
 router.delete('/:id', customerController.deleteCustomer);
 
