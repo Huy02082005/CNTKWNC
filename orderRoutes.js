0231f620363e2e04541589934aecf5fa8267662e
Huy02082005 <duchuy2341@gmail.com>
2025-12-25 21:25:46 +0700

    Update orderRoutes.js

--- orderRoutes.js
+++ orderRoutes.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const orderController = require('../controllers/orderController');
 
 router.get('/', orderController.getAllOrders);
-router.get('/stats', orderController.getOrderStats); // THÊM DÒNG NÀY
+router.get('/stats', orderController.getOrderStats);
 router.get('/:id', orderController.getOrderDetail);
 router.put('/:id/status', orderController.updateOrderStatus);
 
